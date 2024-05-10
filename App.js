fbf28bc387581f6c7302f9a686b41d4973d9fb03
Honza Běhunčík <112960672+honzabehuncik@users.noreply.github.com>
2024-05-10 08:55:56 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>Hello world</Text>
+      <Text>Open up App.js to start!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Press me"
+        color="#f194ff"
+        onPress={() => Alert.alert('Dobry den')}
+      /> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  heading: {
+    fontSize: 120,
+    fontWeight: 500,
+    color: '#fff',
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2d2d30',
     alignItems: 'center',
     justifyContent: 'center',
   },
